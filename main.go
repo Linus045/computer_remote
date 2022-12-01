38f95a46891f50a3a152f65077fe4b66032f2437
Linus045 <lwillms88@gmail.com>
2022-12-01 03:17:55 +0100

    Adds printing of current ip

--- main.go
+++ main.go
@@ -3,18 +3,40 @@ package main
 import (
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os/exec"
 )
 
 func main() {
+	var err error
 	log.Println("Started server")
+	ifaces, err := net.Interfaces()
+
+	var ip net.IP
+	// handle err
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+		// handle err
+		for _, addr := range addrs {
+			switch v := addr.(type) {
+			case *net.IPNet:
+				if v.IP != nil && v.IP.To4() != nil && !v.IP.IsLoopback() && !v.IP.IsUnspecified() {
+					ip = v.IP
+					break
+				}
+			}
+			// process IP address
+		}
+	}
+
+	log.Println("Listeing on : ", ip, ":9090")
 	http.HandleFunc("/", ServeIndex)
 	http.HandleFunc("/pausePlay", pausePlay)
 	http.HandleFunc("/volumeUp", volumeUp)
 	http.HandleFunc("/volumeDown", volumeDown)
 
-	err := http.ListenAndServe(":9090", nil)
+	err = http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
